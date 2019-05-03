6618250f13ddecdaeb9327e73dbeff739af40fc7
daiping2008 <249195022@qq.com>
2019-05-03 16:16:28 +0800

    项目结构目录变更

---
+++ src/route/routes.js
@@ -0,0 +1,15 @@
+import Home from '@/views/Home'
+import About from '@/views/About'
+
+export default [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About
+  }
+]
